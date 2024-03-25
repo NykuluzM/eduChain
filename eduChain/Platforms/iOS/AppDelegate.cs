b0e6fff0f476c4894b1422dcbbf639f0957c64d8
NykuluzM <Carlslasier1@gmail.com>
2024-03-25 20:18:40 +0800

    initial commit

---
+++ eduChain/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace eduChain;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
